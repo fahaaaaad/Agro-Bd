2dd101ba3676ba5f388959d2bc0cc214bbbd2235
hasan1297 <mahamudulhasan1297@gmail.com>
2023-06-21 13:44:18 +0600

    nested dropdown complete

--- partials/_header.php
+++ partials/_header.php
@@ -1,4 +1,3 @@
-
 <script>0</script>
 <!-- Header -->
 
@@ -13,194 +12,297 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
+
+        <!-- Home -->
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
 
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            About Us
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="ourTeam.php">Our team</a></li>
-            <li>
-              <hr class="dropdown-divider">
+
+        <!-- About Us -->
+        <div class="menu-bar" style="padding-left: -15px;">
+          <ul>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#">About Us</a>
+              <div class="dropdown-menu">
+                <ul>
+                  <li><a href="ourTeam.php">Our team</a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a href="#">Contact</a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a href="#">Awards</a>
+                  </li>
+                </ul>
+              </div>
             </li>
-            <li><a class="dropdown-item" href="#"> Contact</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#"> Awards</a></li>
           </ul>
-        </li>
+        </div>
 
 
+
+        <!-- Latest News -->
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="index.html">Latest News</a>
         </li>
 
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Farming Sectors
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#"> Animal Farming</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#"> Fisheries</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#"> Poultry</a></li>
-          </ul>
-        </li>
 
+        <!-- Farming Sectors -->
+        <div class="menu-bar" style="padding-left: -15px;">
+          <ul>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#">Farming
+                Sectors</a>
+              <div class="dropdown-menu">
+                <ul>
+                  <li class="nav-item dropdown"><a href="#">Animal Farming<i class="fas fa-caret-right"></i></a>
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Our Products
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#"> Product Types</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#"> Our Factories/Laboratories</a></li>
-          </ul>
-        </li>
+                    <div class="dropdown-menu-1">
+                      <ul>
+                        <li style="padding-top: 12px;"><a href="cow.php">Cow</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li><a href="#">Goat</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li><a href="#">Buffalo</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li style="padding-bottom: 12px;"><a href="#">Sheep</a></li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a href="#">Fisheries<i class="fas fa-caret-right"></i></a>
 
+                    <div class="dropdown-menu-1">
+                      <ul>
+                        <li style="padding-top: 12px;"><a href="#">Carp</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li style="padding-bottom: 12px;"><a href="#">Prawn</a></li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a href="#">Poultry<i class="fas fa-caret-right"></i></a>
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Our Solutions
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="farming_sector.php">For Farmers</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#"> For Funders</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#"> Agri-Input</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#"> Supply Chain</a></li>
-            <!-- <li>
-            <hr class="dropdown-divider">
-          </li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li> -->
-          </ul>
-        </li>
-
-      </ul>
-          <div class="mx-2">
-
-
-            <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#Loginmodal">
-              Login
-            </button>
-            <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#Loginmodal2">
-              signup
-            </button>
-
-            <!-- Modal -->
-            <!-- login modal -->
-            <div class="modal fade" id="Loginmodal" tabindex="-1" aria-labelledby="LoginmodalLabel" aria-hidden="true">
-              <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="LoginmodalLabel">Agro Bd</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body ">
-                    <form>
-                      <img class="mb-4 center" src="logo.png" alt="" width="72" height="57">
-                      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-                      <div class="form-floating my-3">
-                        <input type="text" class="form-control" id="floatingInput">
-                        <label for="floatingInput">username</label>
-                      </div>
-                      <div class="form-floating my-3">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Password</label>
-                      </div>
-
-                      <div class="form-check text-start my-3">
-                        <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">
-                          Remember me
-                        </label>
-                      </div>
-                      <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-
-
-                    </form>
-
-
-                  </div>
-                  <div class="modal-footer">
-
-                  </div>
-                </div>
+                    <div class="dropdown-menu-1">
+                      <ul>
+                        <li style="padding-top: 12px;"><a href="#">Chicken</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li><a href="#">Duck</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li><a href="#">Pigeons</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li><a href="#">Quail</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li style="padding-bottom: 12px;"><a href="#">Turkey</a></li>
+                      </ul>
+                    </div>
+                  </li>
+                </ul>
               </div>
-            </div>
-
-            <!-- Modal2 -->
-            <!-- Signuplogin modal -->
-            <div class="modal fade" id="Loginmodal2" tabindex="-1" aria-labelledby="LoginmodalLabel" aria-hidden="true">
-              <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="LoginmodalLabel">Agro Bd</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body ">
-                    <!-- form action -->
-                    <form action="" method="post">
-                      <img class="mb-4 center" src="logo.png" alt="" width="72" height="57">
-                      <h1 class="h3 mb-3 fw-normal">Please signup</h1>
-
-                      <div class="form-floating my-3">
-                        <input type="text" class="form-control" id="username" name="username">
-                        <label for="floatingInput">username</label>
-                      </div>
-                      <div class="form-floating my-3">
-                        <input type="password" class="form-control" id="Password" placeholder="Password"
-                          name="password">
-                        <label for="password">Password</label>
-                      </div>
-                      <div class="form-floating my-3">
-                        <input type="text" class="form-control" id="cpassword" name="cpassword">
-                        <label for="cpassword">Confirm Passworf</label>
-                      </div>
-
-                      <button class="btn btn-primary w-100 py-2" type="submit">signup</button>
-
-
-                    </form>
-
-
-                  </div>
-                  <div class="modal-footer">
-
-                  </div>
-                </div>
-              </div>
-            </div>
-            <button type="button" class="btn btn-secondary">En</button>
-            <button type="button" class="btn btn-success">Bd</button>
-          </div>
-
-
+            </li>
+          </ul>
 
         </div>
+
+
+
+        <!-- Our Products -->
+        <div class="menu-bar" style="padding-left: -15px;">
+          <ul>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#">Our Products</a>
+              <div class="dropdown-menu">
+                <ul>
+                  <li class="nav-item dropdown"><a href="#">Product Types<i class="fas fa-caret-right"></i></a>
+                    <div class="dropdown-menu-1">
+                      <ul>
+                        <li style="padding-top: 12px;"><a href="#">Animal Feed</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li><a href="#">Fish Feed</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li><a href="#">Poultry Feed</a></li>
+                      </ul>
+                    </div>
+                  </li>
+
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+
+                  <li><a href="#">Factories/Laboratories</a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+
+        </div>
+
+
+
+        <!-- Our Solutions -->
+        <div class="menu-bar" style="padding-left: -15px;">
+          <ul>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#">Our Solutions</a>
+              <div class="dropdown-menu">
+                <ul>
+                  <li><a href="farming_sector.php">For Farmers</a></li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a href="#"> For Funders</a></li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a href="#"> Agri-Input</a></li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a href="#"> Supply Chain</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+
+      </ul>
+      <div class="mx-2">
+
+
+
+
+        <!-- Right Side Buttons -->
+        <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#Loginmodal">
+          Login
+        </button>
+        <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#Loginmodal2">
+          signup
+        </button>
+
+        <!-- Modal -->
+        <!-- login modal -->
+        <div class="modal fade" id="Loginmodal" tabindex="-1" aria-labelledby="LoginmodalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="LoginmodalLabel">Agro Bd</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body ">
+                <form>
+                  <img class="mb-4 center" src="logo.png" alt="" width="72" height="57">
+                  <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+                  <div class="form-floating my-3">
+                    <input type="text" class="form-control" id="floatingInput">
+                    <label for="floatingInput">username</label>
+                  </div>
+                  <div class="form-floating my-3">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                  </div>
+
+                  <div class="form-check text-start my-3">
+                    <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      Remember me
+                    </label>
+                  </div>
+                  <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+
+
+                </form>
+
+
+              </div>
+              <div class="modal-footer">
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Modal2 -->
+        <!-- Signuplogin modal -->
+        <div class="modal fade" id="Loginmodal2" tabindex="-1" aria-labelledby="LoginmodalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="LoginmodalLabel">Agro Bd</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body ">
+                <!-- form action -->
+                <form action="" method="post">
+                  <img class="mb-4 center" src="logo.png" alt="" width="72" height="57">
+                  <h1 class="h3 mb-3 fw-normal">Please signup</h1>
+
+                  <div class="form-floating my-3">
+                    <input type="text" class="form-control" id="username" name="username">
+                    <label for="floatingInput">username</label>
+                  </div>
+                  <div class="form-floating my-3">
+                    <input type="password" class="form-control" id="Password" placeholder="Password" name="password">
+                    <label for="password">Password</label>
+                  </div>
+                  <div class="form-floating my-3">
+                    <input type="text" class="form-control" id="cpassword" name="cpassword">
+                    <label for="cpassword">Confirm Passworf</label>
+                  </div>
+
+                  <button class="btn btn-primary w-100 py-2" type="submit">signup</button>
+
+
+                </form>
+
+
+              </div>
+              <div class="modal-footer">
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <button type="button" class="btn btn-secondary">En</button>
+        <button type="button" class="btn btn-success">Bd</button>
       </div>
-      </div>
-    </nav>
+
+
+
+    </div>
+  </div>
+  </div>
+</nav>
