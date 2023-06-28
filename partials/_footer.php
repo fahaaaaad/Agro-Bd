6a1f13a5b2b598400f9fd46e040b429a5a481268
hasan1297 <107293762+hasan1297@users.noreply.github.com>
2023-06-28 13:29:41 +0600

    footer modified

--- partials/_footer.php
+++ partials/_footer.php
@@ -3,54 +3,59 @@
   <!-- Footer -->
   <footer class="bg-dark text-center text-white">
     <!-- Grid container -->
-    <div class="container p-4">
+    <div class="container p-3" style="margin-bottom:-35px">
       <!-- Section: Social media -->
 
       <!-- Section: Social media -->
 
-      <!-- Section: Form -->
-      <section class="">
-        <form action="">
-          <!--Grid row-->
-          <div class="row d-flex justify-content-center">
-            <!--Grid column-->
-            <div class="col-auto">
-              <p class="pt-2">
-                <strong>Sign up for our newsletter</strong>
-              </p>
-            </div>
-            <!--Grid column-->
+    <?php
+    // echo "
+    // <!-- Section: Form -->
+    // <section class="">
+    //   <form action="">
+    //     <!--Grid row-->
+    //     <div class="row d-flex justify-content-center">
+    //       <!--Grid column-->
+    //       <div class="col-auto">
+    //         <p class="pt-2">
+    //           <strong>Sign up for our newsletter</strong>
+    //         </p>
+    //       </div>
+    //       <!--Grid column-->
 
-            <!--Grid column-->
-            <div class="col-md-5 col-12">
-              <!-- Email input -->
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="form5Example21" class="form-control" />
-                <label class="form-label" for="form5Example21">Email address</label>
-              </div>
-            </div>
-            <!--Grid column-->
+    //       <!--Grid column-->
+    //       <div class="col-md-5 col-12">
+    //         <!-- Email input -->
+    //         <div class="form-outline form-white mb-4">
+    //           <input type="email" id="form5Example21" class="form-control" />
+    //           <label class="form-label" for="form5Example21">Email address</label>
+    //         </div>
+    //       </div>
+    //       <!--Grid column-->
 
-            <!--Grid column-->
-            <div class="col-auto">
-              <!-- Submit button -->
-              <button type="submit" class="btn btn-outline-light mb-4">
-                Subscribe
-              </button>
-            </div>
-            <!--Grid column-->
-          </div>
-          <!--Grid row-->
-        </form>
-      </section>
-      <!-- Section: Form -->
+    //       <!--Grid column-->
+    //       <div class="col-auto">
+    //         <!-- Submit button -->
+    //         <button type="submit" class="btn btn-outline-light mb-4">
+    //           Subscribe
+    //         </button>
+    //       </div>
+    //       <!--Grid column-->
+    //     </div>
+    //     <!--Grid row-->
+    //   </form>
+    // </section>
+    // <!-- Section: Form -->
+    
+    // ";
+    ?>
 
       <!-- Section: Text -->
-      <section class="mb-4">
+      <section>
         <p>
-          Unlock the potential of your farm with our innovative farming solutions.Experience sustainable agriculture
-          practices for higher yields and a greener future.Empowering farmers with modern technology and expert
-          guidance for agricultural success.
+          Our goal is to empower farmers to grow in the farming sectors
+          <br>
+          Be A Farmer  |  Work Hard  |  Gain Success
         </p>
       </section>
       <!-- Section: Text -->
@@ -64,13 +69,13 @@
     <!-- Grid container -->
 
     <!-- Copyright -->
-    <div class="footer">
+    <div class="footer" style="margin-top: -50px;">
       <div class="footer-content">
 
 
         <!-- About -->
         <div class="p-2 footer-column about-us">
-          <div class="footer-head header-font">
+          <div class="footer-head header-font" style="padding-bottom: 9px;">
             About
           </div>
           <div class="footer-item">
@@ -79,18 +84,12 @@
           <div class="footer-item">
             <a href="#">Career</a>
           </div>
-          <div class="footer-item">
-            <a href="#">Gallery</a>
-          </div>
-          <div class="footer-item">
-            <a href="#">FAQ</a>
-          </div>
         </div>
 
 
         <!-- Legal -->
         <div class="p-2 footer-column legals">
-          <div class="footer-head">
+          <div class="footer-head" style="padding-bottom: 9px;">
             Legal
           </div>
           <div class="footer-item">
@@ -99,19 +98,19 @@
           <div class="footer-item">
             <a href="#">Terms of Services</a>
           </div>
-          <div class="footer-item">
+          <!-- <div class="footer-item">
             <a href="#">Risk Management</a>
-          </div>
+          </div> -->
         </div>
 
 
         <!-- Contact -->
         <div class="p-2 footer-column contact-us">
-          <div class="footer-head">
+          <div class="footer-head" style="padding-bottom: 9px;">
             Contact
           </div>
           <div class="footer-item">
-            <p>
+            <p style="margin:2px">
               <span>Email: </span>
               <a href="mailto:info@ifarmer.asia">
                 info@agro-bd.com
@@ -119,24 +118,25 @@
             </p>
           </div>
           <div class="footer-item">
-            <p><span>For Call: </span>
+            <p style="margin:2px"><span>For Call: </span>
               <a href="tel:+8801302536026">
                 +8801302536026
               </a>
             </p>
           </div>
           <div class="footer-item">
-            <p><span>Calling Hours: </span>Sat -Thurs, 10am-6pm</p>
+            <p style="margin:2px"><span>Calling Hours: </span>Sat -Thurs, 10am-6pm</p>
           </div>
-          <div class="footer-item">
+          <!-- <div class="footer-item">
             <p><span>Address: </span>House - 8E, Road - 81, Gulshan 2, Dhaka 1212</p>
           </div>
           <div class="footer-item">
             <p><span>Visiting Hours: </span>Sun-Thurs,</p>
             (appointment basis)
-          </div>
+          </div> -->
           <div class="footer-item">
-            <div class="footer-social py-2">
+
+            <!-- <div class="footer-social py-2">
               <div class="social-insta">
                 <a href="https://www.instagram.com/ifarmerasia/" target="_blank">
                   <i class="fa fa-instagram gtm-instagram"></i>
@@ -162,13 +162,14 @@
                   <i class="fa fa-linkedin gtm-linkedin"></i>
                 </a>
               </div>
-            </div>
+            </div> -->
+
           </div>
         </div>
 
 
         <!-- Our App -->
-        <div class="p-2 app-links-container footer-column">
+        <!-- <div class="p-2 app-links-container footer-column">
           <div class="footer-head">
             Our App
           </div>
@@ -184,19 +185,19 @@
               </a>
             </div>
           </div>
-        </div>
+        </div> -->
 
 
         <!-- Company Information -->
         <div class="p-2 footer-column company-info">
-          <div class="footer-head">
+          <div class="footer-head" style="padding-bottom: 9px;">
             Company Information
           </div>
-          <div class="footer-item">
-            Trade License Number: 273687
-          </div>
-          <div class="footer-item">
-            BIN Number: 0017302330402
+          <!-- <div class="footer-item" style="">
+            <b>Trade License Number:</b>  273687
+          </div> -->
+          <div class="footer-item" style="">
+            <b>BIN Number:</b>  0017302330402
           </div>
         </div>
 
@@ -204,8 +205,8 @@
       </div>
       <hr>
       <div class="copyright">
-        <div class="copyright-msg pb-5 pt-1 px-2">
-          Copyright © 2019-2023 iFarmer, All Rights Reserved. Call us: <a href="tel:+8801302536026">+8801302536026</a>
+        <div class="copyright-msg pb-1 pt-1 px-1" style="margin-top: -20px;">
+          Copyright © 2019-2023 Agro-BD, All Rights Reserved. Call us: <a href="tel:+8801302536026">+8801853438156</a>
           or send us a mail: <a href="mailto:info@ifarmer.asia">info@agro-bd.com</a>
         </div>
       </div>
