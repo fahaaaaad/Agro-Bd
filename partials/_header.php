d61db429454bca950716d1bb604f088999856869
hasan1297 <107293762+hasan1297@users.noreply.github.com>
2023-07-15 13:23:01 +0600

    in progress

--- partials/_header.php
+++ partials/_header.php
@@ -1,7 +1,10 @@
 <?php
 
   include 'partials/_dbconnect.php';
-  if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+
+  
+  // if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(isset($_POST['signIn'])){
       $username = $_POST['username'];
       $password = $_POST['password'];
@@ -51,6 +54,49 @@
       }  
     }
 
+    // session_start();
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception; 
+
+    
+    //Load Composer's autoloader
+    require 'vendor/autoload.php';
+
+    function sendemail_verify($name, $email, $verify_token){
+      $mail = new PHPMailer(true);
+      //Server settings
+      $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+      $mail->isSMTP();                                            //Send using SMTP
+      $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+      $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+      $mail->Username   = 'user@example.com';                     //SMTP username
+      $mail->Password   = 'secret';                               //SMTP password
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+      $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+      //Recipients
+      $mail->setFrom('from@example.com', 'Mailer');
+      $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+      $mail->addAddress('ellen@example.com');               //Name is optional
+      $mail->addReplyTo('info@example.com', 'Information');
+      $mail->addCC('cc@example.com');
+      $mail->addBCC('bcc@example.com');
+
+      //Attachments
+      $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+      $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+      //Content
+      $mail->isHTML(true);                                  //Set email format to HTML
+      $mail->Subject = 'Here is the subject';
+      $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+      $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+      $mail->send();
+      echo 'Message has been sent';
+    }
+
     if(isset($_POST['signUp'])){
       $name = $_POST['full_name'];
       $username = $_POST['username'];
@@ -70,11 +116,11 @@
         header("Location: index.php");
       }
       else{
-        // Ensert user data
+        // Insert user data
         $sql="INSERT INTO `user` (`id`, `name`, `username`, `email`, `mobile`, `account_type`, `interest`, `photo`, `password`, `Verify_token`, `created_at`) VALUES (NULL, '$name', '$username', '$email', '$mobile', '$account_type', NULL, NULL, '$password', '$verify_token', current_timestamp()) ";
         $result = mysqli_query($conn, $sql);
         if($result){
-          // sendemail
+          sendemail_verify("$name", "$email", "$verify_token");
           $_SESSION['status']="Registration Success! Please verify your email address!!";
           header("Location: index.php");
         }
@@ -84,42 +130,12 @@
         }
       }
     }
-// function generate_email_verification_token() {
-//   // Generate a random email verification token.
-//   $token_length = 32;
-//   $token_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-//   $token = '';
-//   for ($i = 0; $i < $token_length; $i++) {
-//     $token .= $token_characters[rand(0, strlen($token_characters) - 1)];
-//   }
-//   return $token;
-// }
-
-// function send_email_verification($email, $email_verification_token) {
-//   // Send the email verification email.
-//   $subject = 'Please verify your email address';
-//   $message = 'Hi,
-
-// Please click on the link below to verify your email address:
-
-// https://www.yourdomain.com/verify_email.php?token=' . $email_verification_token;
-
-//   mail($email, $subject, $message);
-// }
-
-// if (isset($_POST['email'])) {
-//   // Generate an email verification token.
-//   $email_verification_token = generate_email_verification_token();
-
-//   // Send the email verification email.
-//   send_email_verification($_POST['email'], $email_verification_token);
-
-//   // Redirect the user to the verification page.
-//   header('Location: partials/verify_email.php?token=' . $email_verification_token);
-// }
 
 
-  }
+  // }
+
+
+
 ?>
 <script>0</script>
 <!-- Header -->
